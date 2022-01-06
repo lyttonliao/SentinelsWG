33556121787aab5617f0cfff942a03ffda75f2e6
Lytton Liao <lytton.liao@gmail.com>
2022-01-06 02:33:58 -0500

    Correcting files for Test and Lint during ci-cd

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,6 +7,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
+
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
         db_conn = None
@@ -16,5 +17,6 @@ class Command(BaseCommand):
             except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        
+
         self.stdout.write(self.style.SUCCESS('Database available'))
+        
