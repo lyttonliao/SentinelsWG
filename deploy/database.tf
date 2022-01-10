8b14ce4c98d9e3683df322c44d82b8517268f9e3
Lytton Liao <lytton.liao@gmail.com>
2022-01-10 14:41:39 -0500

    Changed postgres version

--- deploy/database.tf
+++ deploy/database.tf
@@ -31,7 +31,7 @@ resource "aws_db_instance" "main" {
   allocated_storage       = 20
   storage_type            = "gp2"
   engine                  = "postgres"
-  engine_version          = "14.1"
+  engine_version          = "11.4"
   instance_class          = "db.t2.micro"
   db_subnet_group_name    = aws_db_subnet_group.main.name
   password                = var.db_password
