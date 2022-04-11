0b46cf004e30032afe5dcbc8d5294b29963b929d
Lytton Liao <lytton.liao@gmail.com>
2022-04-10 21:05:37 -0400

    Final linting adjustment

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -38,8 +38,10 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'password', 'first_name',
-                'last_name', 'watchlistitems', 'tickers')
+        fields = (
+            'email', 'password', 'first_name',
+            'last_name', 'watchlistitems', 'tickers'
+        )
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
         depth = 2
 
